959b2c56fc0202bd7b2494d4ab7913ba7f075be3
Olga Garder <o.garder@blanc.ru>
2025-05-22 16:46:40 +0300

    ini greeting screen toggle button and text

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
     }
-
-
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greetind", for: .normal
+            )
+        }
 }
 
